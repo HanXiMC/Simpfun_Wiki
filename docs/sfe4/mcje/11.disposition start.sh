f56f0e255336a4f5e82adc5e38b25c71c29dfc9a
HanXiMC <109091020+HanXiMC@users.noreply.github.com>
2023-09-01 13:47:50 +0800

    Update 11.disposition start.sh

--- docs/sfe4/mcje/11.disposition start.sh
+++ docs/sfe4/mcje/11.disposition start.sh
@@ -1,5 +1,5 @@
 ---
-title: 如何配置 start.sh
+如何配置 start.sh
 ---
 
 这里只暂时演示Forge 1.19.2 服务端
@@ -10,11 +10,11 @@ title: 如何配置 start.sh
 
 2.点击文件
 
-3.根据以下路径/libraries/net/minecraftforge/forge/1.19.2-43.2.0
+3.根据以下路径*/libraries/net/minecraftforge/forge/1.19.2-43.2.0*
 
 4.复制“1.19.2-43.2.0”转到start.sh
 
-5.替换“${openjdk17} -Xms1024M -Xmx${result}M @libraries/net/minecraftforge/forge/此处为版本号/unix_args.txt "$@"”中的版本号为“1.19.2-43.2.0”
+5.替换*“${openjdk17} -Xms1024M -Xmx${result}M @libraries/net/minecraftforge/forge/此处为版本号/unix_args.txt "$@"”中的版本号为“1.19.2-43.2.0”*
 
 6.启动服务器即可
 
